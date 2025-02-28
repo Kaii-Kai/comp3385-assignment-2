dda3d6fd306d34c1b3d81504bb7a527a6692d70a
Kyra Lee <500001736@my.fiveislands.uwi.edu>
2025-02-27 23:15:27 -0400

    Modified file

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,5 @@ Route::get('/about', function () {
 Route::get('/feedback', [FeedbackController::class, 'create']);
 
 Route::post('/feedback/send', [FeedbackController::class, 'send']);
+
+Route::get('/feedback/success', [FeedbackController::class, 'success']);
