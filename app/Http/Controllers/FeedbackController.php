3463f254d5f0d55a25c610e54e476849a9451c4a
Kyra Lee <500001736@my.fiveislands.uwi.edu>
2025-02-27 23:13:34 -0400

    Modified file

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use App\Mail\Feedback;
+use Illuminate\Support\Facades\Mail;
 
 class FeedbackController extends Controller
 {
@@ -10,7 +13,24 @@ class FeedbackController extends Controller
 		return view('feedback-form');
 	}
 	
-	public function send(){
-		return view('');
+	public function send(Request $request):RedirectResponse{
+		$validated= $request->validate([
+			'full_name'=> 'required|string|max:100',
+			'email'=> 'required|email',
+			'message'=> 'required',
+		]);
+		
+		$name= $request->input('full_name');
+		$email= $request->input('email');
+		$comment= $request->input('message');
+		
+		Mail::to('comp3385@uwi.edu', 'COMP3385 Course Admin')
+			->send(new Feedback($name, $email, $comment));
+		
+		return redirect('/feedback/success');
+	}
+	
+	public function success(){
+		return view('feedback-success');
 	}
 }
